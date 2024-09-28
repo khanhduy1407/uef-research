b830da26f20990f3db1dc80627b869c40a6835d8
NKDuy <nkduy.dev@gmail.com>
2024-09-28 18:52:29 +0700

    chore: update typescript 5.5

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './global.scss'
+import App from '@/App'
+import '@/global.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <App />
+)
+
